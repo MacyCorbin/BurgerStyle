efa7a690056afb07a363454e81a76208f2c98508
Macy Corbin <macycorbin@Macys-MacBook-Pro.local>
2019-06-04 19:49:12 -0400

    added node packages and created sql data

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE burgers_db;
+
+INSERT INTO burgers (id, burger_name, devoured, date)
+VALUES
+(null, "Cheeseburger", FALSE, null),
+(null, "Bacon Cheeseburger", FALSE, null),
+(null, "Southwest Burger", FALSE, null),
+(null, "Caribbean Burger", FALSE, null),
+(null, "BBQ Burger", FALSE, null),
+(null, "Portobello Mushroom Burger", FALSE, null),
+(null, "Impossible Burger", FALSE, null);
