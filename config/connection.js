94e59aa5f7968ac510e958a9a324283150a14ee6
Macy Corbin <macycorbin@Macys-MacBook-Pro.local>
2019-06-10 18:26:06 -0400

    changed connection method

--- config/connection.js
+++ config/connection.js
@@ -1,35 +1,56 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
+// // Set up MySQL connection.
+// var mysql = require("mysql");
 
-// set up connection to port and database
-var connection = mysql.createConnection({
-  port: 8889,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgerStyle_db"
-});
+// // set up connection to port and database
+// var connection = mysql.createConnection({
+//   port: 8889,
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "burgerStyle_db"
+// });
 
-// Make connection.
-function handleDisconnect(connection) {
-  connection.on('error', function(err) {
-    if (!err.fatal) {
-      return;
-    }
+// // Make connection.
+// function handleDisconnect(connection) {
+//   connection.on('error', function(err) {
+//     if (!err.fatal) {
+//       return;
+//     }
 
-    if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
-      throw err;
-    }
+//     if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+//       throw err;
+//     }
 
-    console.log('Re-connecting lost connection: ' + err.stack);
+//     console.log('Re-connecting lost connection: ' + err.stack);
 
-    connection = mysql.createConnection(connection.config);
-    handleDisconnect(connection);
-    connection.connect();
+//     connection = mysql.createConnection(connection.config);
+//     handleDisconnect(connection);
+//     connection.connect();
+//   });
+// }
+
+// handleDisconnect(connection);
+
+// // Export connection for our ORM to use.
+// module.exports = connection;
+
+
+var mysql = require('mysql');
+var connection;
+
+// For Heroku Deployment vs. Local MySQL Database
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root', // Add your password
+    database : 'burgerStyle_db' // Add your database
   });
 }
 
-handleDisconnect(connection);
 
-// Export connection for our ORM to use.
+// Export the Connection
 module.exports = connection;
